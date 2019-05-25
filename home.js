7aca6f52a848301d6fbd5114325b909bc67ba38b
Omama Zainab <omi.zainy@gmail.com>
2019-05-25 07:40:53 +0500

    website menu page is modified but cart still has to be done

--- home.js
+++ home.js
@@ -16,7 +16,7 @@ function showSlides() {
 // Get the modal
 var modal = document.getElementById('sign-up');
 var modal2 =document.getElementById('sign-in');
-var modal3 =document.getElementById('Deals');
+
 var modal4 =document.getElementById('careers');
 var modal5 =document.getElementById('contact-us');
 
@@ -28,9 +28,7 @@ window.onclick = function(event) {
     else if (event.target == modal2) {
         modal2.style.display = "none";
     }
-    else if (event.target == modal3) {
-        modal3.style.display = "none";
-    }
+    
     else if (event.target == modal4) {
         modal4.style.display = "none";
     }
@@ -39,69 +37,62 @@ window.onclick = function(event) {
     }
 }
 
-//cart 
+
+
+//cart
+
 if (document.readyState == 'loading') {
     document.addEventListener('DOMContentLoaded', ready)
-  } else {
+} else {
     ready()
-  }
-  
-  function ready() {
+}
+
+function ready() {
     var removeCartItemButtons = document.getElementsByClassName('btn-danger')
     for (var i = 0; i < removeCartItemButtons.length; i++) {
         var button = removeCartItemButtons[i]
         button.addEventListener('click', removeCartItem)
     }
-  
+
     var quantityInputs = document.getElementsByClassName('cart-quantity-input')
     for (var i = 0; i < quantityInputs.length; i++) {
         var input = quantityInputs[i]
         input.addEventListener('change', quantityChanged)
     }
-  
+
     var addToCartButtons = document.getElementsByClassName('shop-item-button')
     for (var i = 0; i < addToCartButtons.length; i++) {
         var button = addToCartButtons[i]
         button.addEventListener('click', addToCartClicked)
     }
-  
+
     document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
-  }
-  
-  function purchaseClicked() {
+}
+
+function purchaseClicked() {
     alert('Thank you for your purchase')
     var cartItems = document.getElementsByClassName('cart-items')[0]
     while (cartItems.hasChildNodes()) {
         cartItems.removeChild(cartItems.firstChild)
     }
     updateCartTotal()
-  }
-  
-  function removeCartItem(event) {
-    var buttonClicked = event.target
-    buttonClicked.parentElement.parentElement.remove()
-    updateCartTotal()
-  }
-  
-  function quantityChanged(event) {
+}
+
+function quantityChanged(event) {
     var input = event.target
     if (isNaN(input.value) || input.value <= 0) {
         input.value = 1
     }
     updateCartTotal()
-  }
-  
-  function addToCartClicked(event) {
-    var button = event.target
-    var shopItem = button.parentElement.parentElement
-    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
-    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
-    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
-    addItemToCart(title, price, imageSrc)
-    updateCartTotal()
-  }
-  
-  function addItemToCart(title, price, imageSrc) {
+}
+
+function removeCartItem(event) {
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal();
+}
+
+function addItemToCart(title, price, imageSrc) {
     var cartRow = document.createElement('div')
     cartRow.classList.add('cart-row')
     var cartItems = document.getElementsByClassName('cart-items')[0]
@@ -126,9 +117,9 @@ if (document.readyState == 'loading') {
     cartItems.append(cartRow)
     cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
     cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
-  }
-  
-  function updateCartTotal() {
+}
+
+function updateCartTotal() {
     var cartItemContainer = document.getElementsByClassName('cart-items')[0]
     var cartRows = cartItemContainer.getElementsByClassName('cart-row')
     var total = 0
@@ -142,4 +133,14 @@ if (document.readyState == 'loading') {
     }
     total = Math.round(total * 100) / 100
     document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
-  } 
+}
+
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('product-info-name')[0].innerText
+    var price = shopItem.getElementsByClassName('product-info-price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('product-image')[0].src
+    addItemToCart(title, price, imageSrc)
+    updateCartTotal()
+}
